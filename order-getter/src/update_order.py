bb8990b53aa014f0eea950da6ab0b01de1131a97
John Isley <jisley@bushelpowered.com>
2025-07-31 07:59:47 -0500

    Wrapping call in try catch

--- order-getter/src/update_order.py
+++ order-getter/src/update_order.py
@@ -7,7 +7,9 @@ algorithms = get_algorithms()
 print("Updating order...")
 
 #response = private_api.algo_settings_from_response("EQUIHASH", algorithms)
-response = private_api.set_limit_hashpower_order("51640af6-9834-4001-b782-6f5474e6b990", "0.0060", "EQUIHASH", algorithms)
-
-#response = private_api.set_price_hashpower_order("51640af6-9834-4001-b782-6f5474e6b990", "0.0491", "EQUIHASH", algorithms)
-print(response)
+#response = private_api.set_limit_hashpower_order("51640af6-9834-4001-b782-6f5474e6b990", "0.0060", "EQUIHASH", algorithms)
+try:
+    response = private_api.set_price_hashpower_order("51640af6-9834-4001-b782-6f5474e6b990", "0.0491", "EQUIHASH", algorithms)
+    print(response)
+except Exception as e:
+    print(f"Error updating order: {e}")
